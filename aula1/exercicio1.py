8b729a3ea127622e1295844bb89a449c679c5bbd
Luiz <luizjl>
2023-10-24 19:06:08 -0300

    Atividades aula 1

---
+++ aula1/exercicio1.py
@@ -0,0 +1,44 @@
+
+# Informacoes da primeira pessoa
+nome1 = input("Qual o nome da 1 pessoa?")
+idade1 = input("Idade da 1 pessoa:")
+peso1 = input("Peso da 1 pessoa:")
+altura1 = input("Altura da 1 pessoa")
+
+# Informacoes da segunda pessoa
+nome2 = input("Qual o nome da 2 pessoa?")
+idade2 = input("Idade da 2 pessoa:")
+peso2 = input("Peso da 2 pessoa:")
+altura2 = input("Altura da 2 pessoa")
+
+# Informacoes da terceira pessoa
+nome3 = input("Qual o nome da 3 pessoa?")
+idade3 = input("Idade da 3 pessoa:")
+peso3 = input("Peso da 3 pessoa:")
+altura3 = input("Altura da 3 pessoa")
+
+print("|" + "="*50 + "|")
+
+print(f"Pessoa1")
+print(f'Nome 1: {nome1} ')
+print(f'Idade 1: {idade1} ')
+print(f'Peso 1: {peso1} ')
+print(f'Altura 1: {altura1} ')
+
+print("|" + "="*50 + "|")
+
+print(f"Pessoa2")
+print(f'Nome 2: {nome2} ')
+print(f'Idade 2: {idade2} ')
+print(f'Peso 2: {peso2} ')
+print(f'Altura 2: {altura2} ')
+
+print("|" + "="*50 + "|")
+
+print(f"Pessoa3")
+print(f'Nome 3: {nome3} ')
+print(f'Idade 3: {idade3} ')
+print(f'Peso 3: {peso3} ')
+print(f'Altura 3: {altura3} ')
+
+print("|" + "="*50 + "|")
---
+++ aula1/main.py
@@ -0,0 +1,5 @@
+
+dia, mes, ano = 23, 10, 2023
+
+data = "Data: %i/%i/%i"%(dia, mes ,ano)
+print (data.upper())
